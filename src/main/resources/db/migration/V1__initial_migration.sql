5e40196e2cdef0919082bf1db603170ef6559616
Shivaxm <shivam.patel951@yahoo.com>
2025-11-05 19:50:29 -0500

    Add products table

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -12,4 +12,18 @@ CREATE TABLE users (
                        updated_at timestamptz DEFAULT now()
 );
 
+CREATE TABLE products (
+                          id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+                          name TEXT NOT NULL,
+                          slug TEXT NOT NULL UNIQUE,
+                          description TEXT,
+                          price_cents BIGINT NOT NULL CHECK (price_cents >= 0),
+                          currency VARCHAR(3) NOT NULL DEFAULT 'USD',
+                          sku TEXT NOT NULL UNIQUE,
+                          inventory_qty INT NOT NULL DEFAULT 0 CHECK (inventory_qty >= 0),
+                          is_active BOOLEAN NOT NULL DEFAULT TRUE,
+                          created_at timestamptz DEFAULT now(),
+                          updated_at timestamptz DEFAULT now()
+);
+
 
