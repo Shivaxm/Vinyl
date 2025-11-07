e36c2178b90ee0789e3b49102b39bdefe25f548f
Shivaxm <shivam.patel951@yahoo.com>
2025-11-07 14:06:06 -0500

    Setting up Jwt configuration

---
+++ src/main/java/com/shivam/store/config/JwtConfig.java
@@ -0,0 +1,22 @@
+package com.shivam.store.config;
+
+import io.jsonwebtoken.security.Keys;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.jwt")
+@Data
+public class JwtConfig {
+    private String secret;
+    private int accessExpiration;
+    private int refreshExpiration;
+
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+}
